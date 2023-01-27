58eaa5b7a19b4c537cf309882b2649252a68dfcb
Kirill Karpovich <kir.karpovich@gmail.com>
2023-01-27 18:17:39 +0300

    Initial project

---
+++ GitHubFollowers/FavoriteListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteListVC.swift
+//  GitHubFollowers
+//
+//  Created by Kirill Karpovich on 27.01.23.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Kirill Karpovich on 27.01.23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+    }
+    
+
+
+}
