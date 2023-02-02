3fd284e32234311c9745b6efeebcc155d86434aa
Kirill Karpovich <kir.karpovich@gmail.com>
2023-02-02 17:27:57 +0300

    User Info child view

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Kirill Karpovich on 2.02.23.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
